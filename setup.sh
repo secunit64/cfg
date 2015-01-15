75c7929362b4d47fc88aff5352f8d2b1f85bbd95
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2015-01-15 14:00:15 +0300

    Added .gitconfig links

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -sf ~/cfg/.bashrc ~/.bashrc
 ln -sf ~/cfg/.bash_profile ~/.bash_profile
 ln -sf ~/cfg/.emacs.d ~/.emacs.d
 ln -sf ~/cfg/.ssh/config ~/.ssh/config
+ln -sf ~/cfg/.gitconfig ~/.gitconfig
 cd ~/.ssh && chmod 644 config
 cd ~/cfg/
 ln -sf ~/cfg/.liquidprompt/liquidprompt ~/.liquidprompt
