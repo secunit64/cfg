807888d4a7e832b6eb1707e8374a1921dbda38e0
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2016-03-15 12:06:21 +0300

    updated setup scripts

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf ~/develop/cfg/.bash_profile ~/.bash_profile
 ln -sf ~/develop/cfg/.emacs.d ~/.emacs.d
 ln -sf ~/develop/cfg/.ssh/config ~/.ssh/config
 ln -sf ~/develop/cfg/.gitconfig ~/.gitconfig
+ln -sf ~/develop/cfg/.aws ~/.aws
 cd ~/.ssh && chmod 644 config
 cd ~/develop/cfg/
 ln -sf ~/develop/cfg/.liquidprompt/liquidprompt ~/.liquidprompt
